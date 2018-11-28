66a7a3be9d41fd87012002053ca98140a60fab0c
Fei Gao <fgao1@ualberta.ca>
2018-11-28 11:09:16 -0700

    wip

--- starter-files/public/dist/App.bundle.js
+++ starter-files/public/dist/App.bundle.js
@@ -63,12 +63,11 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 3);
+/******/ 	return __webpack_require__(__webpack_require__.s = 2);
 /******/ })
 /************************************************************************/
 /******/ ([
-/* 0 */,
-/* 1 */
+/* 0 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -98,21 +97,21 @@ exports.$ = $;
 exports.$$ = $$;
 
 /***/ }),
-/* 2 */
+/* 1 */
 /***/ (function(module, exports) {
 
 // removed by extract-text-webpack-plugin
 
 /***/ }),
-/* 3 */
+/* 2 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 
-__webpack_require__(2);
+__webpack_require__(1);
 
-var _bling = __webpack_require__(1);
+var _bling = __webpack_require__(0);
 
 /***/ })
 /******/ ]);
