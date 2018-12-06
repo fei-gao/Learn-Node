4aea3e4cd5e662e9248c7b212f3689b214c495f9
Fei Gao <fgao1@ualberta.ca>
2018-12-06 12:06:00 -0700

    user can edit their name and email

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -45,4 +45,6 @@ router.post('/register',
 
 router.get('/logout', authController.logout);
 
+router.get('/account', authController.isLoggedIn, userController.account);
+router.post('/account', catchErrors(userController.updateAccount));
 module.exports = router;
